303ab088cdce7053eaba7ee271e4da7ac062a71b
arsalan <arsab7879@gmail.com>
2022-08-26 21:12:43 +0430

    setup main page components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
+import Directory from "./components/directory/directory";
+
 const App = () => {
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
+
   return (
-    <div className="categories=container">
-      <div className="category-container">
-        {/*<image />*/}
-        <div className="category-body-container">
-          <h2>hats</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/*<image />*/}
-        <div className="category-body-container">
-          <h2>jackets</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/*<image />*/}
-        <div className="category-body-container">
-          <h2>sneakers</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/*<image />*/}
-        <div className="category-body-container">
-          <h2>womens</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/*<image />*/}
-        <div className="category-body-container">
-          <h2>mens</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-
-    </div>
-  );
+    <Directory categories={categories}/>
+  )
 }
 
 export default App;
