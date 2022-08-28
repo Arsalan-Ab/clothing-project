c26b80eb3790568c20d26df2e01a1ce35b703e01
arsalan <arsab7879@gmail.com>
2022-08-28 12:35:31 +0430

    impl sign-in with firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopup,createUserDocumentFromAuth } from "../../utils/firbase/firbase"
+
 const SignIn = () => {
+const logGoogleUser = async() => {
+    const {user} = await signInWithGooglePopup()
+    const userDocRef = await createUserDocumentFromAuth(user)
+}
+
     return (
         <div>
             <h2>hello</h2>
+            <button onClick={logGoogleUser}> sing in google</button> 
         </div>
     )
 }
---
+++ src/utils/firbase/firbase.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7LK_PlZQUabyj5ujbE3H55KW25qM7kok",
+    authDomain: "clothing-project-c36fb.firebaseapp.com",
+    projectId: "clothing-project-c36fb",
+    storageBucket: "clothing-project-c36fb.appspot.com",
+    messagingSenderId: "1086390720962",
+    appId: "1:1086390720962:web:6bcff8cad8d466eb19de5b"
+};
+
+const firbaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+
+
+
+export const db = getFirestore()
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db,'users',userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    if(!userSnapshot.exists()) {
+        const {displayName,email} = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return userDocRef
+    
+}
