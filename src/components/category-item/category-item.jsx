2b8633209dbdbbf4a7d0101f8b8f036cf6d262f9
arsalan <arsab7879@gmail.com>
2022-10-12 12:20:08 +0330

    fix main page style

--- src/components/category-item/category-item.jsx
+++ src/components/category-item/category-item.jsx
@@ -3,14 +3,14 @@ import './category-item.styles.scss'
 const CategoryItem = ( {category} ) => {
   const { title, imageUrl } = category
   return (
-    <div className="category-container">
+    <div className="directories-container">
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`
         }}>
       </div>
-      <div className="category-body-container">
+      <div className="directory-body-container">
         <h2>{title}</h2>
         <p>shop now</p>
       </div>
